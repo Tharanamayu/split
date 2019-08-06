82bddd1c653013472778e2ad984cbbabaa16fcce
Tharana <tharana.maya@gmail.com>
2019-08-06 20:52:47 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'split';
+  serverElements = [{type:'server' , name:'Testserver' , content: 'just a test'},{type:'server' , name:'Testserver' , content: 'just a test'}];
+  onServerAdded(serverdata: {serverName: string , serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverdata.serverName,
+      content: serverdata.serverContent
+    });
+  }
+  onBlueprintAdded(blueprintData: {serverName: string , serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
